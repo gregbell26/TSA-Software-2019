0d9fe79da70189c4656dcc4b284494d5c5787c72
TriHardStudios <trihard.studios@outlook.com>
2019-09-16 17:03:09 -0600

    Wrote CMAKE file for macOS that links corretly this time!

--- main.cpp
+++ main.cpp
@@ -1,17 +1,37 @@
-//
-// Created by jimmy on 9/11/2019.
-//
+//Test File for compilation
 
-#include "main.h"
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
+
 #include <iostream>
-#include <string>
 
+int main() {
+    glfwInit();
 
-int main(){
-    std::string usrName;
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
 
-    std::cout << "Please enter your name: ";
-    std::cin>> usrName;
-    std::cout << "Welcome to Acrylic2D, " << usrName << "!" << std::endl;
+    uint32_t extensionCount = 0;
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
 
+    std::cout << extensionCount << " extensions supported" << std::endl;
+
+    glm::mat4 matrix;
+    glm::vec4 vec;
+    auto test = matrix * vec;
+
+    while(!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+
+    glfwTerminate();
+
+    return 0;
 }
