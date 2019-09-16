d489278ca6bc0a7622dd3494c498b703d583cafe
TriHardStudios <trihard.studios@outlook.com>
2019-09-16 11:05:26 -0600

    Wrote Basic Sanity Check Ignored Build Pathes

--- main.cpp
+++ main.cpp
@@ -3,3 +3,15 @@
 //
 
 #include "main.h"
+#include <iostream>
+#include <string>
+
+
+int main(){
+    std::string usrName;
+
+    std::cout << "Please enter your name: ";
+    std::cin>> usrName;
+    std::cout << "Welcome to Acrylic2D, " << usrName << "!" << std::endl;
+
+}
