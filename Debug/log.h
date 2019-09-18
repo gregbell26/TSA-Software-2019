c3db983c4bbf3745a845c8566dec73ed00ae4379
Camille Menendez <cmmenendez01@bvsd.org>
2019-09-18 17:14:43 -0600

    Created prototype functions for logger class

--- Debug/log.h
+++ Debug/log.h
@@ -5,4 +5,29 @@
 #ifndef TSA_SOFTWARE_2019_LOG_H
 #define TSA_SOFTWARE_2019_LOG_H
 
+#include <iostream>
+#include <fstream>
+#include <string>
+class Logger{
+private :
+    std::ifstream fin;
+    std::ofstream fout;
+    bool usingFile;
+    bool usingConsole;
+    enum ERROR_LEVEL{
+        INFORMATION = 0, WARNING, ERROR, FATAL;
+    };
+    int activeErrorLevel = INFORMATION;
+public :
+    Logger ();
+    Logger(ERROR_LEVEL errorLevel, bool usingFile, bool usingConsole);
+    void Log();
+
+};
+
+
+
+
+
+
 #endif //TSA_SOFTWARE_2019_LOG_H
