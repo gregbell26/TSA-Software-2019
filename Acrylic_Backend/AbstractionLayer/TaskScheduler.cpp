634120ebcf406a3c0e2a576733a0a3ec10915c83
geb92011 <37025708+geb92011@users.noreply.github.com>
2019-10-07 08:34:34 -0600

    Merge taskscheduler

--- Acrylic_Backend/AbstractionLayer/TaskScheduler.cpp
+++ Acrylic_Backend/AbstractionLayer/TaskScheduler.cpp
@@ -1,24 +1,31 @@
 // Created by Gabriel Bergeson oct. 2019
 
-#include "TaskScheduler.hpp"
+#include "taskScheduler.hpp"
 
 
-void TaskScheduler::addTask() {
-
-}
-
-int TaskScheduler::createTask() {
-    return 0;
-}
-
-void TaskScheduler::threadRun() {
+// Gets the function pointer, elapse time and       start time if applicable
+void passTask()
+{
 
 }
 
-int TaskScheduler::assessQueue() {
-    return 0;
-}
+// Makes a structure from the passed information
+int creatTask()
+{
 
-void TaskScheduler::terminateThread() {
+}
+// Runs the threads with an infinite loop checking the queue and executing threads
+void threadRun()
+{
+
+}
+// Checks queue to see if there is space for any threads
+int assesQueue()
+{
+
+}
+// Stops a thread
+void terminateThread()
+{
 
 }
