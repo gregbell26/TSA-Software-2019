adc07f910322c2cfc31823418b9c0e62f18e47d6
jimmybolt <44655345+jimmybolt@users.noreply.github.com>
2019-10-09 18:04:20 -0600

    Started on fileSytem level Json parsing and made a file for an example json

---
+++ Acrylic_Backend/BackendAPI/FileSystem/acrylic_fileSystem.hpp
@@ -0,0 +1,22 @@
+#include <fstream>
+#include <string>
+#include <filesystem>
+#include <list>
+#include "C:/SDKS/json-develop/include/nlohmann/json.hpp"
+namespace fs = std::filesystem;
+
+
+std::string gameDataPath;
+
+
+std::string readJSONs(){
+    std::fstream jsonStream;
+
+    for(const auto& file : fs::directory_iterator(gameDataPath)){
+        jsonStream.open(gameDataPath + file.path().string());
+
+        jsonStream.close();
+    }
+    std::fstream stream;
+
+}
