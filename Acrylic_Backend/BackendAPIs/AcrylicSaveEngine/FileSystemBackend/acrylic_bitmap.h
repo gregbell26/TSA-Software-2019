fa2f932461767e725b5da0b44dd41efb396d34e7
Cameron <coolheathboys@gmail.com>
2019-11-06 16:33:40 -0700

    Bitmap

---
+++ Acrylic_Backend/BackendAPIs/AcrylicSaveEngine/FileSystemBackend/acrylic_bitmap.h
@@ -0,0 +1,14 @@
+//
+// Created by coolh on 11/6/2019.
+//
+
+#ifndef ACRYLIC2D_ACRYLIC_BITMAP_H
+#define ACRYLIC2D_ACRYLIC_BITMAP_H
+
+
+class acrylic_bitmap {
+
+};
+
+
+#endif //ACRYLIC2D_ACRYLIC_BITMAP_H
