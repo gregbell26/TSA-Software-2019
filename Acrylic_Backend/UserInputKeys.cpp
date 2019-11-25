9c8015af41eb876937ff5fdf0d416c19c6b219d3
Corbin <33209092+westes07@users.noreply.github.com>
2019-11-25 11:03:57 -0700

    test user input keys

--- Acrylic_Backend/UserInputKeys.cpp
+++ Acrylic_Backend/UserInputKeys.cpp
@@ -9,20 +9,20 @@
 struct keyInfo {
     int key;
     int action;
-    void (func*);
+    void (*func)(int);
 };
 
 std::vector<keyInfo> keys;
 
-void addKeyMap(int key, int action, void (*func)){
-    keys.push_back({key, action, func});
+void addKeyMap(int key, int action, void (*func)(int)){
+    keys.push_back(keyInfo{key, action, func});
 }
 
 static void callback(GLFWwindow* window, int key, int scanCode, int action, int mods){
     int count = 0;
     for (int i = 0; i < keys.size(); ++i) {
         if (keys.at(i).key == key){
-            *keys.at(i).func;
+            keys.at(i).func(action);
             count++;
         }
     }
@@ -34,5 +34,4 @@ static void callback(GLFWwindow* window, int key, int scanCode, int action, int 
 
 void initialize(){
     glfwSetKeyCallback(Plexi::getWindowRef(), callback);
-
 }
--- Acrylic_Backend/main.nfp.cpp
+++ Acrylic_Backend/main.nfp.cpp
@@ -1,8 +1,13 @@
 #include "./BackendAPIs/Plexi2DRenderer/acrylic_plexiRenderer_core.hpp"
 #include "./BackendAPIs/AcrylicSaveEngine/FileSystemBackend/acrylic_fileSystem.hpp"
+#include "UserInputKeys.cpp"
+
+void doTheThing(int times){
+    std::cout << "hi: " << times<< std::endl;
+}
 
 int main(){
-//    Plexi::initPlexi();
+    Plexi::initPlexi();
     readJSON('a');
-
+    addKeyMap(27, 82, doTheThing);
 }
