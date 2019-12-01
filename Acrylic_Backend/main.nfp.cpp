1b2332d8e41040800fc7b98f5e344749fcf022d2
TriHard Studios <trihard.studios@outlook.com>
2019-11-30 23:02:25 -0700

    Wrote stupid test case with flashy colors when ever you press an input

--- Acrylic_Backend/main.nfp.cpp
+++ Acrylic_Backend/main.nfp.cpp
@@ -3,22 +3,24 @@
 #include "UserInput.cpp"
 #include <iostream>
 UserInput::Returns doTheThing(int times){
-    std::cout << "hi: " << times<< std::endl;
+    Plexi::onUpdate();
+    glfwSwapBuffers(Plexi::getWindowRef());
     return {};
 }
 
 UserInput::Returns scroll(double i, double j){
-    std::cout << "Scroll " << i << ", " << j << std::endl;
-    return {};
+    Plexi::onUpdate();
+    glfwSwapBuffers(Plexi::getWindowRef());    return {};
 }
 int main(){
     Plexi::initPlexi();
     UserInput::initialize();
     UserInput::addKeyMap(GLFW_KEY_W, GLFW_PRESS, doTheThing);
-    UserInput::addKeyMap(GLFW_KEY_A, GLFW_PRESS, nullptr);
+    UserInput::addKeyMap(GLFW_KEY_A, GLFW_PRESS, doTheThing);
     UserInput::addKeyMap(GLFW_KEY_S, GLFW_PRESS, doTheThing);
     UserInput::addKeyMap(GLFW_KEY_D, GLFW_PRESS, doTheThing);
     UserInput::setScrollFunc(scroll);
+    UserInput::setMouseRightFunc(GLFW_MOUSE_BUTTON_LEFT, doTheThing);
     while(!glfwWindowShouldClose(Plexi::getWindowRef())){
         glfwPollEvents();
     }
