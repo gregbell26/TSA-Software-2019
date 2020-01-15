100c15bf1c45f8ac4c790d0389c3ce49a906aaff
jimmybolt <44655345+jimmybolt@users.noreply.github.com>
2020-01-15 13:25:05 -0700

    Got arrays working and fixed some bugs

--- Acrylic_Backend/BackendAPIs/AcrylicSaveEngine/FileSystemBackend/acrylic_fileSystem.hpp
+++ Acrylic_Backend/BackendAPIs/AcrylicSaveEngine/FileSystemBackend/acrylic_fileSystem.hpp
@@ -47,9 +47,132 @@ namespace A2D::Filesystem::Loaders::Json {
         std::map<std::string, float> fmap;
         std::map<std::string, std::string> smap;
         std::map<std::string, jsonMaps> jmap;
+
+        std::map<std::string, int*> iamap;
+        std::map<std::string, float*> famap;
+        std::map<std::string, std::string*> samap;
+        std::map<std::string, jsonMaps*> jamap;
     };
 
+    void interpretArray(std::string toArr, std::string key, jsonMaps* where);
+    jsonMaps interpretJson(std::string stringJson);
+
+    void interpretArray(std::string toArr, std::string key, jsonMaps* where){
+
+        std::cout << toArr << "\n";
+        if(!toArr[0])
+            return;
+        int elem = 0;
+        int size = 0;
+        int i = 0;
+        bool notInQuotes = 1;
+        unsigned loc = 0;
+        unsigned next = -1;
+
+        int* iaval;
+        float* faval;
+        std::string* saval;
+        jsonMaps* javal;
+
+        do {
+            next = toArr.find_first_of("{}\",", next + 1);
+            if(next == -1)
+                break;
+
+            if (toArr[next] == '{' && notInQuotes)
+                i++;
+            else if(toArr[next] == '}' && notInQuotes)
+                i--;
+            else if(toArr[next] == '\"')
+                notInQuotes = !notInQuotes;
+            else if(i == 0 && notInQuotes)
+                size ++;
+
+        } while (true);
+        size++;
+        std::cout << "size:" << size << "\n";
+
+        next = toArr.find_first_of(',', loc);
+
+
+
+        if (toArr[loc] == '{') {
+            javal = new jsonMaps[size];
+            elem = 0;
+            while( elem < size && loc != -1) {
+                if(loc != 0)
+                    loc++;
+                i = 1;
+                next = loc;
+                notInQuotes = 1;
+                do {
+
+                    next = toArr.find_first_of("{}\"", next + 1);
+
+                    if (toArr[next] == '{' && notInQuotes)
+                        i++;
+                    else if(toArr[next] == '}' && notInQuotes)
+                        i--;
+                    else if(toArr[next] == '\"')
+                        notInQuotes = !notInQuotes;
+
+                } while (i != 0);
+                std::cout << loc << " " << next << "\n";
+                std::cout << toArr.substr(loc, next - loc + 1) << "\n";
+                std::cout << key << " -j- " << "{" << "\n";
+                javal[elem] = interpretJson(toArr.substr(loc, next - loc + 1));
+                std::cout << "}" << "\n";
+
+                elem++;
+                loc = toArr.find_first_of(",",next);
+            }
+            next++;
+            where->jamap.insert_or_assign(key, javal);
+        }else if (toArr[loc] == '\"') {
+            saval = new std::string[size];
+            elem = 0;
+            while( elem < size) {
+                std::cout << loc << "\n";
+                saval[elem] = toArr.substr(loc + 1, next - loc - 2);
+                std::cout << key << " -s- " << saval[elem] << "\n";
+                loc = next + 1;
+                next = toArr.find_first_of(",",next+1);
+                if(next == -1)
+                    next = toArr.length();
+                elem++;
+            }
+            where->samap.insert_or_assign(key, saval);
+
+        } else if (toArr.find_first_of('.', loc) != -1 && toArr.find_first_of('.', loc) < next) {
+            faval = new float[size];
+            elem = 0;
+            while( elem < size) {
+                std::cout << loc << "\n";
+                faval[elem] = std::stof(toArr.substr(loc, next - loc));
+                std::cout << key << " -d- " << faval[elem] << "\n";
+                loc = next + 1;
+                next = toArr.find_first_of(",",next+1);
+                elem++;
+            }
+            where->famap.insert_or_assign(key, faval);
+        } else {
+            iaval = new int[size];
+            elem = 0;
+            while( elem < size) {
+                std::cout << loc << "\n";
+                iaval[elem] = std::stoi(toArr.substr(loc, next - loc));
+                std::cout << key << " -i- " << iaval[elem] << "\n";
+                loc = next + 1;
+                next = toArr.find_first_of(",",loc);
+                elem++;
+            }
+            where->iamap.insert_or_assign(key, iaval);
+        }
+
+    }
+
     jsonMaps interpretJson(std::string stringJson) {
+//        std::cout << stringJson << "\n";
         jsonMaps maps;
         std::string tempJson;
         tempJson.assign(stringJson);
@@ -57,6 +180,7 @@ namespace A2D::Filesystem::Loaders::Json {
         size_t mid = 0;
         size_t end = 0;
         std::string key;
+        bool notInQuotes = 1;
         int ival;
         float fval;
         std::string sval;
@@ -75,19 +199,20 @@ namespace A2D::Filesystem::Loaders::Json {
         if (tempJson[tempJson.length() - 1] == '}')
             tempJson.erase(tempJson.length() - 1, 1);
 
-//        std::cout << tempJson << "\n";
+        std::cout << tempJson << "\n";
 
         do {
             loc++;
             mid = tempJson.find_first_of(':', loc);
-            end = tempJson.find_first_of(',', loc);
+            end = tempJson.find_first_of(',', mid);
             if (mid == -1)
                 break;
             if (end == -1)
                 end = tempJson.length();
 
-//            std::cout << mid << " " << end << "\n";
-//            std::cout << tempJson.find_first_of(".", mid, end - mid ) << "\n";
+            std::cout << mid << " " << end << "\n";
+            //std::cout << tempJson[mid] << " " << tempJson[end] << "\n";
+            std::cout << tempJson.find_first_of(",", mid, end - mid ) << "\n";
             key = tempJson.substr(loc, mid - loc - 1);
             if (key[0] == '\"')
                 key.erase(0, 1);
@@ -97,40 +222,60 @@ namespace A2D::Filesystem::Loaders::Json {
             if (tempJson[mid + 1] == '{') {
                 i = 0;
                 end=mid;
+                notInQuotes = 1;
                 do {
-                    end = tempJson.find_first_of("{}", end + 1);
+                    end = tempJson.find_first_of("{}\"", end + 1);
 
-                    if(tempJson[end] == '{' )
+                    if (tempJson[end] == '{' && notInQuotes)
+                        i++;
+                    else if(tempJson[end] == '}' && notInQuotes)
+                        i--;
+                    else if(tempJson[end] == '\"')
+                        notInQuotes = !notInQuotes;
+
+                }while(i!=0);
+                std::cout << key << " -j- " << "{" << "\n";
+                jval = interpretJson(stringJson.substr(mid+2,end-mid));
+                end++;
+                std::cout << "}" << "\n";
+                maps.jmap.insert_or_assign(key, jval);
+            }else if(tempJson[mid + 1] == '['){
+                i = 0;
+                end=mid;
+                do {
+                    end = tempJson.find_first_of("[]", end + 1);
+                    if(tempJson[end] == '[' )
                         i++;
                     else
                         i--;
                 }while(i!=0);
-//                std::cout << key << " -j- " << "{" << "\n";
-                jval = interpretJson(stringJson.substr(mid+2,end-mid));
+                std::cout << key << " -a- " << "[" << "\n";
+                interpretArray(tempJson.substr(mid+2,end-mid-2),key,&maps);
                 end++;
-//                std::cout << "}" << "\n";
-                maps.jmap.insert_or_assign(key, jval);
+                std::cout << "]" << "\n";
             }
             else if (tempJson[mid + 1] == '\"') {
                 sval = tempJson.substr(mid + 2, end - mid - 3);
-//                std::cout << key << " -s- " << sval << "\n";
+                std::cout << key << " -s- " << sval << "\n";
                 maps.smap.insert_or_assign(key, sval);
             } else if (tempJson.find_first_of('.', mid) != -1 && tempJson.find_first_of('.', mid) < end) {
                 fval = std::stof(tempJson.substr(mid + 1, end - mid - 1));
-//                std::cout << key << " -d- " << fval << "\n";
+                std::cout << key << " -d- " << fval << "\n";
                 maps.fmap.insert_or_assign(key, fval);
             } else {
                 ival = std::stoi(tempJson.substr(mid + 1, end - mid - 1));
-//                std::cout << key << " -i- " << ival << "\n";
+                std::cout << key << " -i- " << ival << "\n";
                 maps.imap.insert_or_assign(key, ival);
             }
-            loc = tempJson.find_first_of(',', loc);
-//            std::cout << loc << "\n\n";
+            loc = end;
+            std::cout << loc << "\n\n";
         } while (loc != -1);
 
         return maps;
     }
 }
+
+
 
 
 #endif //ACRYLIC_FILESYSYTEM_HPP
