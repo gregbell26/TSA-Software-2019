6d8d1c2c7af01d686af732d7866dbb9cbf7eff45
jimmybolt <44655345+jimmybolt@users.noreply.github.com>
2020-01-18 11:53:55 -0700

    Added an fps counter

---
+++ Acrylic_Backend/frameTimer.h
@@ -0,0 +1,36 @@
+//
+// Created by jimmy on 1/18/2020.
+//
+
+#ifndef ACRYLIC2D_PLSXIFRAMETIMER_H
+#define ACRYLIC2D_PLSXIFRAMETIMER_H
+
+#include <chrono>
+
+namespace Timer{
+    class frameTimer {
+        public:
+        std::chrono::time_point<std::chrono::system_clock> start;
+        void startTimer(){
+            start = std::chrono::system_clock::now();
+        }
+        double getTime(){
+            std::chrono::time_point<std::chrono::system_clock> end;
+            end = std::chrono::system_clock::now();
+
+            std::chrono::duration<double> elapsed_seconds = end - start;
+            return elapsed_seconds.count();
+        }
+        double getFPS(unsigned frames){
+
+            std::chrono::time_point<std::chrono::system_clock> end;
+            end = std::chrono::system_clock::now();
+
+            std::chrono::duration<double> elapsed_seconds = end - start;
+
+            return frames / elapsed_seconds.count();
+        }
+    };
+}
+
+#endif //ACRYLIC2D_PLSXIFRAMETIMER_H
