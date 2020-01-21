53df4d49a9ed1935a464f6d745b7027f57056f0f
Cameron <coolheathboys@gmail.com>
2020-01-20 17:41:42 -0700

    updated variable names 2 to make Jesse and Gabe happy and added commets

--- Acrylic_Backend/acrylic_random.cpp
+++ Acrylic_Backend/acrylic_random.cpp
@@ -7,7 +7,7 @@
     std::random_device generator; // how generates the random number
     std::uniform_int_distribution <int> distribution(0,256); // range for RGB
     std::uniform_int_distribution <int> distribute(0,99); // range for SV
-    std::uniform_int_distribution <int> distrib(0,359); // range for H
+    std::uniform_int_distribution <int> distrib(0,360); // range for H
 
     glm::vec3 A2D::Random::Random::ranRGB()
     {
@@ -69,39 +69,39 @@
         float green;
         float blue;
         float colorIntensity = brightness * saturation;
-        float fHPrime = fmod(hue / 60.0, 6); // 
-        float fX = colorIntensity * (1 - fabs(fmod(fHPrime, 2) - 1)); //
-        float fM = brightness - colorIntensity; // added to red green and blue to complete conversion
+        float RemainderHue =  fmod(hue / 60.0, 6); // remainder of (hue/60)/6
+        float Intensity2 = colorIntensity * (1 - fabs(fmod(RemainderHue, 2) - 1)); // intensity*(1-|remainder(remain/2)-1|)
+        float IntensityOffset = brightness - colorIntensity; // added to red green and blue to complete conversion
 
-        if(0 <= fHPrime && fHPrime < 1) {
-            red = colorIntensity;
-            green = fX;
+        if(0 <= RemainderHue && RemainderHue < 1) { // checks the remainder of hue
+            red = colorIntensity; // sets red,green, blue colors
+            green = Intensity2;
             blue = 0;
         }
-        else if(1 <= fHPrime && fHPrime < 2) {
-            red = fX;
+        else if(1 <= RemainderHue && RemainderHue < 2) {
+            red = Intensity2;
             green = colorIntensity;
             blue = 0;
         }
-        else if(2 <= fHPrime && fHPrime < 3) {
+        else if(2 <= RemainderHue && RemainderHue < 3) {
             red = 0;
             green = colorIntensity;
-            blue = fX;
+            blue = Intensity2;
         }
-        else if(3 <= fHPrime && fHPrime < 4) {
+        else if(3 <= RemainderHue && RemainderHue < 4) {
             red = 0;
-            green = fX;
+            green = Intensity2;
             blue = colorIntensity;
         }
-        else if(4 <= fHPrime && fHPrime < 5) {
-            red = fX;
+        else if(4 <= RemainderHue && RemainderHue < 5) {
+            red = Intensity2;
             green = 0;
             blue = colorIntensity;
         }
-        else if(5 <= fHPrime && fHPrime < 6) {
+        else if(5 <= RemainderHue && RemainderHue < 6) {
             red = colorIntensity;
             green = 0;
-            blue = fX;
+            blue = Intensity2;
         }
         else {
             red = 0;
@@ -109,10 +109,10 @@
             blue = 0;
         }
 
-        red += fM;
-        green += fM;
-        blue += fM;
-        return glm::vec3(red, green, blue);
+        red += IntensityOffset;
+        green += IntensityOffset;
+        blue += IntensityOffset;
+        return glm::vec3(red, green, blue); // returns converted RGB
     }
 
 
