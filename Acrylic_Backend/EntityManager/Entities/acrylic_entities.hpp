c39eb132cb8b153f0b9921e4c1bbfa42b59952ac
jimmybolt <44655345+jimmybolt@users.noreply.github.com>
2020-03-04 16:55:34 -0700

    Basic templates for entity types

--- Acrylic_Backend/EntityManager/Entities/acrylic_entities.hpp
+++ Acrylic_Backend/EntityManager/Entities/acrylic_entities.hpp
@@ -5,6 +5,17 @@
 
 namespace A2D::EntityManager {
 
+    class Entity{
+        public:
+        
+
+        protected:
+        ///texture stuff
+
+        int xPosition;
+        int yPosition;
+    };
+
     template<typename T, typename R>
     class UpdatableEntity {
     public:
@@ -17,6 +28,39 @@ namespace A2D::EntityManager {
         std::function<R> m_onUpdate;
 
     };
+
+
+    struct Hit_Box{
+        int xSize;
+        int ySize;
+        int xPos;
+        int yPos;
+    };
+
+    class InteractableEntity{
+        public:
+
+
+        protected:
+        Hit_Box hitBox;
+
+
+    };
+
+    struct Movement_Data{
+        int xVelocity;
+        int yVelocity;
+        int xAcceleration;
+        int yAcceleration;
+    };
+
+    class PhysicsObject{
+        public:
+
+        protected:
+        Movement_Data movementData;
+    };
+
 }
 
 
